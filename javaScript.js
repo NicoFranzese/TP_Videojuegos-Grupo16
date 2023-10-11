60f6e7c936718c70b7113ca2dee9c1aced117570
Nico Franzese <franzesens@gmail.com>
2023-10-11 08:38:56 -0300

    Se sube proyecto con plantilla base de como va a ser la página y navegación, con paleta de colores elegida

---
+++ javaScript.js
@@ -0,0 +1,13 @@
+<script>
+	// Función para abrir el popup
+	function openPopup() {
+		document.getElementById("overlay").style.display = "block";
+		document.getElementById("popup").style.display = "block";
+	}
+
+	// Función para cerrar el popup
+	function closePopup() {
+		document.getElementById("overlay").style.display = "none";
+		document.getElementById("popup").style.display = "none";
+	}
+</script>
